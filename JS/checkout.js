0cd2a16e5fcb86679479ab10e33193673fca392a
Lebo04 <nobomvukelebogile497@gmail.com>
2023-06-09 11:34:01 +0200

    fianls

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,4 +1,5 @@
 let cart = JSON.parse(localStorage.getItem("cart"));
+let dress = JSON.parse(localStorage.getItem('dresses'));
 
 async function displayContent() {
   let data = document.querySelector(".cartItems");
@@ -10,7 +11,8 @@ async function displayContent() {
   });
     document.querySelector(
       ".outs"
-    ).innerHTML += `<p class="total">Total <span class="total" style="color:black"><b></b></span></p>`;
+    ).innerHTML += ` <p>Shipping fee R100</p>
+    <p class="total">Total <span class="total" style="color:black"><b></b></span></p>`;
 }
 
 displayContent();
@@ -18,8 +20,17 @@ displayContent();
 function calTotal() {
   let total = 0;
   cart.forEach((e) => {
-    total += e.price * e.quantity;
+    total += (e.price * e.quantity) + 100;
   });
   document.querySelector(".total").innerHTML += `R${total}`;
 }
 calTotal();
+
+function calPrice() {
+  let total = 0;
+  
+    total += dress.price * dress.quantity;
+
+  document.querySelector(".price").innerHTML += `R${total}`;
+}
+calPrice();
